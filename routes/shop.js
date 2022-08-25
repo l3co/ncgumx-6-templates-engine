0676c58d283126a935886daaf0a280009e0838c7
leco <lcosta.sp.br@gmail.com>
2022-08-24 22:01:23 -0300

    refactor(handlebar): :art: configure the controllers to return some settings to page, the settings is to apply css templates

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,13 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' })
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    productCSS: true,
+    shopMenu: true,
+    path: '/'
+  })
 });
 
 module.exports = router;
